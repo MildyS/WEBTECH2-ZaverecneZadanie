e4963ceb6d56def47bd057b432b012e84063a499
MildyS <ilovsky.ilovsky@gmail.com>
2023-05-07 11:50:41 +0200

    Teacher - fily a image hodené do tabuľky. Časy sú stále mimo

--- zp/resources/views/teacher.blade.php
+++ zp/resources/views/teacher.blade.php
@@ -24,56 +24,77 @@
         </table>
         <br>
         <h2>Uploaded Files:</h2>
-        <ul>
-            <!-- ... -->
+        <table class="table">
+            <thead>
+            <tr>
+                <th>File Name</th>
+                <th>Status</th>
+                <th>Actions</th>
+            </tr>
+            </thead>
+            <tbody>
             @foreach($latexFiles as $file)
-                <li>
-                    {{ $file->file_name }}
-                    @if($file->publish_at)
-                        @if($file->publish_at >= now()->addHours(2))
-                            (Will be published at: {{ $file->publish_at }})
+                <tr>
+                    <td>{{ $file->file_name }}</td>
+                    <td>
+                        @if($file->publish_at)
+                            @if($file->publish_at >= now())
+                                Will be published at: {{ $file->publish_at }}
+                            @else
+                                Published at: {{ $file->publish_at }}
+                            @endif
                         @else
-                            (Published at: {{ $file->publish_at }})
+                            Published date not set yet.
                         @endif
-                    @else
-                        (Published date not set yet.)
-                    @endif
-                    <form action="{{ route('teacher.delete', $file->id) }}" method="POST" style="display: inline;">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit" class="btn btn-danger btn-sm">Delete</button>
-                    </form>
-                    <form action="{{ route('teacher.togglePublish', $file->id) }}" method="POST" style="display: inline;">
-                        @csrf
-                        <button type="submit" class="btn btn-sm {{ $file->is_published ? 'btn-warning' : 'btn-success' }}">
-                            {{ $file->is_published ? 'Unpublish' : 'Publish' }}
-                        </button>
-                    </form>
-                    <form action="{{ route('teacher.setPublishDate', $file->id) }}" method="POST" style="display: inline;">
-                        @csrf
-                        <input type="datetime-local" name="publish_at" value="{{ optional($file->publish_at)->format('Y-m-d\TH:i') }}" min="{{ now()->format('Y-m-d\TH:i') }}">
-                        <button type="submit" class="btn btn-sm btn-info">
-                            Set Publish Date
-                        </button>
-                    </form>
-                </li>
+                    </td>
+                    <td>
+                        <form action="{{ route('teacher.delete', $file->id) }}" method="POST" style="display: inline;">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger btn-sm">Delete</button>
+                        </form>
+                        <form action="{{ route('teacher.togglePublish', $file->id) }}" method="POST" style="display: inline;">
+                            @csrf
+                            <button type="submit" class="btn btn-sm {{ $file->is_published ? 'btn-warning' : 'btn-success' }}">
+                                {{ $file->is_published ? 'Unpublish' : 'Publish' }}
+                            </button>
+                        </form>
+                        <form action="{{ route('teacher.setPublishDate', $file->id) }}" method="POST" style="display: inline;">
+                            @csrf
+                            <input type="datetime-local" name="publish_at" value="{{ optional($file->publish_at)->format('Y-m-d\TH:i') }}" min="{{ now()->format('Y-m-d\TH:i') }}">
+                            <button type="submit" class="btn btn-sm btn-info">
+                                Set Publish Date
+                            </button>
+                        </form>
+                    </td>
+                </tr>
             @endforeach
-
-        </ul>
+            </tbody>
+        </table>
         <br>
         <h2>Uploaded Images:</h2>
-        <ul>
+        <table class="table">
+            <thead>
+            <tr>
+                <th>Image Name</th>
+                <th>Action</th>
+            </tr>
+            </thead>
+            <tbody>
             @foreach($images as $image)
-                <li>
-                    {{ $image->file_name }}
-                    <form action="{{ route('teacher.deleteImage', $image->id) }}" method="POST">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit" class="btn btn-danger btn-sm">Delete</button>
-                    </form>
-                </li>
+                <tr>
+                    <td>{{ $image->file_name }}</td>
+                    <td>
+                        <form action="{{ route('teacher.deleteImage', $image->id) }}" method="POST" style="display: inline;">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger btn-sm">Delete</button>
+                        </form>
+                    </td>
+                </tr>
             @endforeach
-        </ul>
+            </tbody>
+        </table>
         <br>
         <br>
     </div>
