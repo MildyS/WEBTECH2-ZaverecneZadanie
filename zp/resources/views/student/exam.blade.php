54d1c724267007921e777e74fd4a1830cbf7a8b5
MildyS <ilovsky.ilovsky@gmail.com>
2023-05-11 22:01:50 +0200

    Vzorce sa zobrazuju spravne (Použitý KaTeX)

--- zp/resources/views/student/exam.blade.php
+++ zp/resources/views/student/exam.blade.php
@@ -8,12 +8,30 @@
         <h2>Problem:</h2>
         <p>{{ $task->task }}</p>
 
-        <h2>Solution:</h2>
-        <p>{{ $task->solution }}</p>
-
         @if($task->images)
-            <h2>Images:</h2>
             <img src="{{ asset('/storage/latex_files' . $task->images) }}" alt="Problem image">
         @endif
+        <br>
+        <br>
+        <h2>Solution:</h2>
+        <p>${{ $task->solution }}$</p>
+
     </div>
+
 @endsection
+
+@push('scripts')
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.7/dist/katex.min.css" integrity="sha384-3UiQGuEI4TTMaFmGIZumfRPtfKQ3trwQE2JgosJxCnGmQpL/lJdjpcHkaaFwHlcI" crossorigin="anonymous">
+    <script defer src="https://cdn.jsdelivr.net/npm/katex@0.16.7/dist/katex.min.js" integrity="sha384-G0zcxDFp5LWZtDuRMnBkk3EphCK1lhEf4UEyEM693ka574TZGwo4IWwS6QLzM/2t" crossorigin="anonymous"></script>
+    <script defer src="https://cdn.jsdelivr.net/npm/katex@0.16.7/dist/contrib/auto-render.min.js" integrity="sha384-+VBxd3r6XgURycqtZ117nYw44OOcIax56Z4dCRWbxyPt0Koah1uHoK0o4+/RRE05" crossorigin="anonymous"></script>
+    <script>
+        document.addEventListener("DOMContentLoaded", function() {
+            renderMathInElement(document.body, {
+                delimiters: [
+                    {left: "$$", right: "$$", display: true},
+                    {left: "$", right: "$", display: false},
+                ]
+            });
+        });
+    </script>
+@endpush
