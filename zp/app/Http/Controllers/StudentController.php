716beb03b75f63eed5c8b541cfda4e35b81aec2a
EunikaFarkasova <91488275+EunikaFarkasova@users.noreply.github.com>
2023-05-15 21:28:24 +0200

    Úprava studentController na viac problémov

--- zp/app/Http/Controllers/StudentController.php
+++ zp/app/Http/Controllers/StudentController.php
@@ -25,16 +25,33 @@ class StudentController extends Controller
 
     public function showExam(Request $request)
     {
-        // Get a random problem
-        $task = Task::inRandomOrder()->first();
+        // Get the selected files from the session
+        $selectedFiles = $request->session()->get('selected_files');
 
-        // If no problem found (e.g., database is empty), redirect back to the student dashboard with an error message
-        if(!$task) {
+        // Initialize an empty array to hold the tasks
+        $tasks = [];
+
+        // For each selected file, get a random task
+        foreach ($selectedFiles as $fileId) {
+            // Get a random task from the current file
+            $task = Task::where('latex_file_id', $fileId)->inRandomOrder()->first();
+
+            // If no task found (e.g., all tasks from this file have been used), continue to the next file
+            if(!$task) {
+                continue;
+            }
+
+            // Add the task to the tasks array
+            $tasks[] = $task;
+        }
+
+        // If no tasks found at all, redirect back to the student dashboard with an error message
+        if(empty($tasks)) {
             return redirect()->route('student')->with('error', 'No problems available at the moment.');
         }
 
-        // Pass the problem to the view
-        return view('student.exam', ['task' => $task]);
+        // Pass the tasks to the view
+        return view('student.exam', ['tasks' => $tasks]);
     }
 
 }
