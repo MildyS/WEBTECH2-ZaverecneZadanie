bc895c8b4f771a6eb867728b9610c11d7d875169
MildyS <ilovsky.ilovsky@gmail.com>
2023-05-18 18:59:26 +0200

    pridanie basic manualu s popisom

--- zp/resources/views/manual.blade.php
+++ zp/resources/views/manual.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
     <div class="container">
-
+        <p style="color: white">YO CUS LUDIA, ZDRAVIM VSETTKYCH CO TOTO CITAJU, BOL TO <b>PAIN</b> PERFEKTNY MANUAL TU BUDE</p>
     </div>
 
 @endsection
