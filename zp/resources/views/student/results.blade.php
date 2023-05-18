a1b03e70b0d16b65d210bb7bfc0ae68bbc519016
xplavy <43574208+peto3601@users.noreply.github.com>
2023-05-18 13:39:49 +0200

    Pridanie results page

--- zp/resources/views/student/results.blade.php
+++ zp/resources/views/student/results.blade.php
@@ -1,8 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
+
     <div class="container">
-    <p>{{ $correctAnswers }}</p>
+        <h1>Exam Results</h1>
+        <div>
+            Points scored: {{ $correctPoints }}
+        </div>
+        <div>
+            Total possible points: {{ $totalPoints }}
+        </div>
+
+        <a href="{{ route('student') }}" class="btn btn-primary">Back to Home</a>
     </div>
 
 @endsection
